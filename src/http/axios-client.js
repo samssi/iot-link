0492cdbbb9b10c49d347d4a07950f39c572bd117
samssi <samir.hamadi@gmail.com>
2017-12-02 21:27:41 +0200

    quotes

--- src/http/axios-client.js
+++ src/http/axios-client.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 const sensorClient = () => {
     return axios.create({
-        baseURL: 'http://localhost:5000/',
+        baseURL: "http://localhost:5000/",
         timeout: 1000
     });
 }
